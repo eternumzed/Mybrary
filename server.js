59697f245cd3f72e7c25e0fd731269c670917402
eternumzed <jcraft174@gmail.com>
2024-10-24 08:30:32 +0800

    initial author routes

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded( {limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
@@ -20,13 +23,14 @@ mongoose.set('strictQuery', false);
 console.log("NODE_ENV:", process.env.NODE_ENV);
 console.log("Database URL:", process.env.DATABASE_URL);
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
